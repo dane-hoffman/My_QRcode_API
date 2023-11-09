0c84af6eca30cc82a66193b809c944ddfe2d9a86
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-08 21:25:34 -0700

    Used Math.random to correlate to the QR code data, required by google qrcode api, to serve as a ticket number. Qr codes last for 3 days; perfect for events.

--- code.js
+++ code.js
@@ -1,26 +1,43 @@
+// Select the 'main' element and the 'header' element from the HTML document
 const main = document.querySelector('#main');
 const headerElement = document.querySelector('header');
 
-let userClicksButton = () => {
-    //Create the button
-    let userButton = document.createElement('button');
-    userButton.textContent = `Click Me!`;
-    headerElement.append(userButton);
+// Create a button element
+let button = document.createElement('button');
+button.textContent = 'Click Me!'; // Set the button text
+headerElement.append(button); // Append the button to the header
 
-    //When the button is clicked
-    //Fetch a new QR code from the API
-    //Create the QR code within the Main element
-    let createQrCodeOnMain = () => {
-        let outPutOnMain = document.createElement('div');
-    
-         outPutOnMain.innerHTML =`
-         <h4>Hello World!</h4>
-         `;
-         
-         main.append(outPutOnMain);
-    };
-    
-    createQrCodeOnMain ();
+// Add a click event listener to the button
+button.addEventListener('click', async () => {
+    try {
+        // Generate a random number between 1 and 100 and set it as qrCodeData
+        const qrCodeData = Math.floor(Math.random() * 100) + 1;
+        
+        // Construct the URL for a QR code with the random number as data
+        const url = `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=${qrCodeData}`;
+        
+        // Create an image element with the QR code as its source
+        const imgElement = document.createElement('img');
+        imgElement.src = url;
 
-};
-userClicksButton();
+        // Create a QR code display within the 'main' element
+        let createQrCodeOnMain = () => {
+            let outPutOnMain = document.createElement('div');
+            
+            // Set the content of the 'h4' element to display the QR code data
+            outPutOnMain.innerHTML = `<h4>QR Code Data: ${qrCodeData}</h4>`;
+            
+            // Clear the existing content of the 'main' element
+            main.innerHTML = '';
+            
+            // Append the QR code image and the new content to the 'main' element
+            outPutOnMain.appendChild(imgElement);
+            main.appendChild(outPutOnMain);
+        };
+
+        // Call the createQrCodeOnMain function to update the content in the 'main' element
+        createQrCodeOnMain();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
