42e42b3860304373198f285456dc876706f99a3b
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-08 19:20:31 -0700

    Initial commit before getting too far and realizing I really wished I did.

---
+++ code.js
@@ -0,0 +1,12 @@
+const main = document.querySelector('main');
+console.log(main);
+const headElement = document.querySelector('h1');
+console.log(headElement);
+
+const createQrCodeinMain = () => {
+    const outPutOnMain = document.createElement('p')
+    const outPutOnMain = "Hello World!";
+    console.log(output);
+}
+
+helloWorld ();
